29958a2a8c23413eaa415412f4ab073e271203d7
matsui123 <matsui121214@gmail.com>
2020-11-30 08:46:24 +0900

    component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,22 @@ import React,{Component} from 'react';
 
 
 
-class App extends Component {
+/*class App extends Component {
   render (){
   return <button type="button" onClick={() => {window.alert("hi")}} />;
   }
+}*/
+const App = () => {
+  return (
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+  )
 }
 
+const Cat = () => {
+  return (<div>Meow!</div>)
+}
 export default App;
