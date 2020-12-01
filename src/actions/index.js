80d7b2ace8c83a5b2533e8cfa33c85f116333e28
matsui123 <matsui121214@gmail.com>
2020-12-02 08:31:50 +0900

    learn action

---
+++ src/actions/index.js
@@ -0,0 +1,10 @@
+export const INCLEMENT = 'INCLEMENT'
+export const DECLEMENT = 'DECLEMENT'
+
+export const increment = () =>({
+  type: INCLEMENT
+})
+
+export const decrement = () =>({
+  type: DECLEMENT
+})
