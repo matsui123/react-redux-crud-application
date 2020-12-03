2a07deed3eb3a174320aec3d7935214846d57e4b
matsui123 <matsui121214gmail.com>
2020-12-04 07:54:55 +0900

    learn reducer

---
+++ src/reducers/count.js
@@ -0,0 +1,15 @@
+import {INCLEMENT, DECLEMENT} from '../actions'
+
+const initialState = {value : 0}
+
+//stateは値を持っていないので初期値を代入
+//最初に作ったinitialstateを代入
+//Reducer すべての分岐処理が書かれている関数
+export default (state = initialState ,action) => {
+  switch(action.type){
+    case INCLEMENT:
+      return {state.value +1}
+    case DECLEMENT:
+      return {state.value-1}
+  }
+}
